4475b49f9c1e1c728d465df9861ce3b036349dcc
杨彪 <000111aaaim@gmail.com>
2021-10-26 19:23:20 +0800

    Update change_gitlab_password.py

--- roles/gitlab/templates/change_gitlab_password.py
+++ roles/gitlab/templates/change_gitlab_password.py
@@ -7,15 +7,14 @@ if __name__=="__main__":
     password_confirm_string="user.password_confirmation='{}'\n".format(password)
     print(bytes(password_setting_string,'utf-8'))
     result=subprocess.Popen(["/bin/bash"], stdin=subprocess.PIPE, bufsize = 1)
-    is_finish=False;
-    while is_finish==False:
-      result.stdin.write(b"gitlab-rails console\n")
-      result.stdin.write(b"user = User.find_by_username 'root'\n")
-      result.stdin.write(bytes(password_setting_string,'utf-8'))
-      result.stdin.write(bytes(password_confirm_string,'utf-8'))
-      result.stdin.write(b'user.save!\n')
-      result.stdin.write(b'exit\n');
-      is_finish=True;
+    os.environ["is_finish"]=0;
+    result.stdin.write(b"gitlab-rails console\n")
+    result.stdin.write(b"user = User.find_by_username 'root'\n")
+    result.stdin.write(bytes(password_setting_string,'utf-8'))
+    result.stdin.write(bytes(password_confirm_string,'utf-8'))
+    result.stdin.write(b'user.save!\n')
+    result.stdin.write(b'exit\n');
+    os.environ["is_finish"]=1;
 
 
     os.remove(sys.argv[0])
