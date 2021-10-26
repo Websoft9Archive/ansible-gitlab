e3745b515d9fb87c5d4ec37b416c7f0f016b40f1
杨彪 <000111aaaim@gmail.com>
2021-10-26 22:00:29 +0800

    Update change_gitlab_password.py

--- roles/gitlab/templates/change_gitlab_password.py
+++ roles/gitlab/templates/change_gitlab_password.py
@@ -7,14 +7,14 @@ if __name__=="__main__":
     password_confirm_string="user.password_confirmation='{}'\n".format(password)
     print(bytes(password_setting_string,'utf-8'))
     result=subprocess.Popen(["/bin/bash"], stdin=subprocess.PIPE, bufsize = 1)
-    os.environ["is_finish"]="false";
     result.stdin.write(b"gitlab-rails console\n")
+    result.stdin.write(b'sleep 300\n');
     result.stdin.write(b"user = User.find_by_username 'root'\n")
     result.stdin.write(bytes(password_setting_string,'utf-8'))
     result.stdin.write(bytes(password_confirm_string,'utf-8'))
     result.stdin.write(b'user.save!\n')
     result.stdin.write(b'exit\n');
-    os.environ["is_finish"]="true";
+    
 
 
     os.remove(sys.argv[0])
