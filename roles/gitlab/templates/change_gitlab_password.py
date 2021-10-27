e56346a396507e40d600b3ba7fba28fdb4e6846c
杨彪 <000111aaaim@gmail.com>
2021-10-27 10:20:24 +0800

    Update change_gitlab_password.py

--- roles/gitlab/templates/change_gitlab_password.py
+++ roles/gitlab/templates/change_gitlab_password.py
@@ -6,7 +6,6 @@ if __name__=="__main__":
     password=sys.argv[1];
     password_setting_string="user.password='{}'\n".format(password);
     password_confirm_string="user.password_confirmation='{}'\n".format(password)
-    print(bytes(password_setting_string,'utf-8'))
     result=subprocess.Popen(["/bin/bash"], stdin=subprocess.PIPE, stdout=subprocess.PIPE,)
     result.stdin.write(b"gitlab-rails console\n")
     result.stdin.write(b"user = User.find_by_username 'root'\n")
@@ -14,10 +13,9 @@ if __name__=="__main__":
     result.stdin.write(bytes(password_confirm_string,'utf-8'))
     result.stdin.write(b'user.save!\n')
     result.stdin.write(b'exit\n');
-    subprocess.run(["sleep","5"])
-    time.sleep(5)
-    while str(result.communicate()).find("true")==-1:
+    while str(result.communicate()).find("exit")==-1:
           subprocess.run(["sleep","5"])# if currennt thread isn`t sleep 5 second ,then the current terminal will have a long duration
           time.sleep(5)
-    print(str(result.communicate()));
+       #   print(str(result.communicate()));
+          print(str(result.stdout.readlines()))
     os.remove(sys.argv[0])
